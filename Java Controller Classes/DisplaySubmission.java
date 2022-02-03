818bde213e24804b28821f340a481202f0dabec9
Chris Collins <70338951+FLAMEGHOST86@users.noreply.github.com>
2022-02-03 13:06:13 -0800

    Update DisplaySubmission.java

--- Java Controller Classes/DisplaySubmission.java
+++ Java Controller Classes/DisplaySubmission.java
@@ -49,9 +49,9 @@ public class DisplaySubmission extends HttpServlet {
 		Connection c = null;
         try
         {
-            String url = "jdbc:mysql://cs3.calstatela.edu/cs3220stu11?useSSL=false&allowPublicKeyRetrieval=true";
-            String username = "cs3220stu11";
-            String password = "StUtfZGU19Bd";
+            String url = ""; // Add your database URL here
+            String username = ""; // Add your username here
+            String password = ""; // Add your password here
             c = DriverManager.getConnection(url, username, password);
             Statement stmt = c.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM submissions WHERE assignment_id = '" + assignmentParam + "';");           
